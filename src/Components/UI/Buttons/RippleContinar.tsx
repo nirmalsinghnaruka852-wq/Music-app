b27e7adbee586adbfd8020cfb6b8210d145eb3d1
Nirmal Singh Naruaka <nirmalsinghnaruka852@gmail.com>
2025-12-26 15:11:04 +0530

    new commit

--- src/Components/UI/Buttons/RippleContinar.tsx
+++ src/Components/UI/Buttons/RippleContinar.tsx
@@ -1,56 +1,66 @@
+import { Animated, GestureResponderEvent, LayoutChangeEvent, Pressable, ViewProps } from "react-native";
+import { useAnimatedValues } from "../../../Hook/useAnimatedValues";
 import { ReactNode, useRef, useState } from "react";
-import { Alert, GestureResponderEvent, Pressable ,Animated ,View, LayoutChangeEvent} from "react-native";
-// import { View } from "react-native/types_generated/index";
 
-type RippleContainerProps = {
-  children: ReactNode;
-};
 
-function RippleContainer({ children }: RippleContainerProps){
-    const postionInX = useRef(0)
-    const postionInY = useRef(0)
-    const opacity = useRef(new Animated.Value(0)).current
-    const scale  = useRef(new Animated.Value(0)).current
+type RippleContainerProps=ViewProps&{
+  children : ReactNode,
+  rippleOpacity :number, 
+  rippleColor:string
+} 
+
+
+function RippleContainer({ children, rippleColor = 'rgba(0,0,0,0.2)', rippleOpacity = 0.5 ,...props }: RippleContainerProps) {
+    
+    const [position, setPosition] = useState<Record<'X' | 'Y', number>>({X: 0,Y: 0,});
+    const opacity = useAnimatedValues(0);
+    const scale = useAnimatedValues(0);
     const [layout, setLayout] = useState({ width: 0, height: 0 });
 
-  const onLayout = (e: LayoutChangeEvent) => {
-    const { width, height } = e.nativeEvent.layout;
-    setLayout({ width, height });
-  };
-   const Handler =(event:GestureResponderEvent)=>{
-    postionInX.current= event.nativeEvent.locationX
-    postionInY.current = event.nativeEvent.locationY
-    scale .setValue(0);
-    opacity.setValue(0.5);
+      const onLayout = (e: LayoutChangeEvent) => {
+        const { width, height } = e.nativeEvent.layout;
+        setLayout({ width, height });
+      };
 
-  
-      Animated.parallel([
-      Animated.timing(scale , { toValue: 1, duration: 400, useNativeDriver: true }),
-      Animated.timing(opacity, { toValue: 0, duration: 400, useNativeDriver: true }),
-    ]).start();
+      const Handler = (event: GestureResponderEvent) => {
+        setPosition({X: event.nativeEvent.locationX, Y:event.nativeEvent.locationX});
+        scale.setValue(0);
+        opacity.setValue(rippleOpacity);
 
-   }
+          Animated.parallel([
+            Animated.timing(scale, {
+              toValue: 1,
+              duration: 400,
+              useNativeDriver: true,
+            }),
+            Animated.timing(opacity, {
+              toValue: 0,
+              duration: 400,
+              useNativeDriver: true,
+            }),
+          ]).start();
+      };
 
   return (
-     <Pressable onPress={Handler} onLayout={onLayout} style={{
-      overflow:'hidden'
-     }}>{children}
-      <Animated.View  style={{
-        position:'absolute',
-         top: postionInY.current - layout.width / 2,
-          left:  postionInX.current- layout.width / 2,
-         width: layout.width,
+    <Pressable onPress={Handler} onLayout={onLayout} style={{ overflow: 'hidden', position: 'relative' }}>
+      <Animated.View
+        {...props}
+        style={{
+          position: 'absolute',
+          top: position.Y - layout.width / 2,
+          left: position.Y - layout.width / 2,
+          width: layout.width,
           height: layout.width,
           borderRadius: layout.width / 2,
-          backgroundColor: 'rgba(0,0,0,0.2)',
-          transform: [{ scale   }],
+          backgroundColor: rippleColor, 
+          transform: [{ scale }],
           opacity,
-
-      }}>
-
-      </Animated.View>
-     </Pressable>
+        }}
+      />
+      {children}
+    </Pressable>
   );
 }
+
 
 export default RippleContainer;
