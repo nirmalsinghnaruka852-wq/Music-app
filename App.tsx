a6c9e2ca8da2537bc2e20b327bf25e82a53917e8
Mustak khan <mustak.coc786@gmail.com>
2025-12-29 22:52:39 +0530

    Refactor RippleContainer

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import './global.css';
 import ThemeProvider, { useTheme } from './src/Providers/ThemeProvider';
 import ThemeText from './src/Providers/ThemeProvider/Components/ThemeText';
 import ThemeView from './src/Providers/ThemeProvider/Components/ThemeView';
-import RippleContainer from './src/Components/UI/Buttons/RippleContinar';
+import RippleContainer from './src/Components/Core/RippleContinar';
 
 export default function App() {
   return (
@@ -25,9 +25,11 @@ export default function App() {
         <ThemeText color='warning' fontSize={8 * 2} className="text-4xl">
           Warning Text
         </ThemeText>
-          <RippleContainer ><ThemeText color='warning' fontSize={8 * 2} className="text-4xl">
+        <RippleContainer >
+            <ThemeText color='warning' fontSize={8 * 2} className="text-4xl">
           click MEsdfgsdfgsdfgsdsadfsdafsdffdsfg
-        </ThemeText></RippleContainer>
+          </ThemeText>
+        </RippleContainer>
         <ThemeView color='primary'  className='w-full h-4' />
         <ThemeView color='secondary'  className='w-full h-4' />
         <ThemeView color='error'  className='w-full h-4' />
--- src/Hook/useAnimatedValues.ts
+++
@@ -1,9 +0,0 @@
-import { useRef } from "react"
-import {Animated} from "react-native"
-
-type Animate =  Animated.Value
-
-export function useAnimatedValues (val:number ):Animate{
-     const variable = useRef<Animate>(new Animated.Value(val))
-     return variable.current 
-}
