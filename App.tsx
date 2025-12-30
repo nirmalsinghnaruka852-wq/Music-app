aee305e045b8df91b92a74319e7fe5e6569f05f0
Mustak khan <mustak.coc786@gmail.com>
2025-12-30 21:01:00 +0530

    Refactor and enhance RippleContainer component
    Renamed RippleContinar.tsx to RippleContainer.tsx and updated all references. Improved the ripple effect by using useRef for precise position calculation, adjusted default ripple opacity, and refined animation scaling and opacity logic. Wrapped App content in RippleContainer for consistent ripple feedback.

--- App.tsx
+++ App.tsx
@@ -3,33 +3,36 @@ import './global.css';
 import ThemeText from './src/Stores/Theme/Components/ThemeText';
 import ThemeView from './src/Stores/Theme/Components/ThemeView';
 import { useThemeHandlers, useThemeStore } from './src/Stores/Theme';
+import RippleContainer from './src/Components/Core/RippleContainer';
 
 export default function App() {
   return (
-    <ThemeView useWindBackground className="flex-1 items-center justify-center bg-red-100">
-      <ToggleTheme/>
-      <ThemeText color='primary' fontSize={8 * 2} className="text-4xl">
-        Primary Text
-      </ThemeText>
-      <ThemeText color='secondary' fontSize={8 * 2} className="text-4xl">
-        Secondary Text
-      </ThemeText>
-      <ThemeText color='error' fontSize={8 * 2} className="text-4xl">
-        Error Text
-      </ThemeText>
-      <ThemeText color='info' fontSize={8 * 2} className="text-4xl">
-        Info Text
-      </ThemeText>
-      <ThemeText color='warning' fontSize={8 * 2} className="text-4xl">
-        Warning Text
-      </ThemeText>
-      
-      <ThemeView color='primary'  className='w-full h-4' />
-      <ThemeView color='secondary'  className='w-full h-4' />
-      <ThemeView color='error'  className='w-full h-4' />
-      <ThemeView color='info'  className='w-full h-4' />
-      <ThemeView color='warning'  className='w-full h-4' />
-    </ThemeView>
+    <RippleContainer className='flex-1 items-center justify-center w-full' >
+      <ThemeView useWindBackground className="flex-1 items-center justify-center w-full">
+        <ToggleTheme/>
+        <ThemeText color='primary' fontSize={8 * 2} className="text-4xl">
+          Primary Text
+        </ThemeText>
+        <ThemeText color='secondary' fontSize={8 * 2} className="text-4xl">
+          Secondary Text
+        </ThemeText>
+        <ThemeText color='error' fontSize={8 * 2} className="text-4xl">
+          Error Text
+        </ThemeText>
+        <ThemeText color='info' fontSize={8 * 2} className="text-4xl">
+          Info Text
+        </ThemeText>
+        <ThemeText color='warning' fontSize={8 * 2} className="text-4xl">
+          Warning Text
+        </ThemeText>
+        
+        <ThemeView color='primary'  className='w-full h-4' />
+        <ThemeView color='secondary'  className='w-full h-4' />
+        <ThemeView color='error'  className='w-full h-4' />
+        <ThemeView color='info'  className='w-full h-4' />
+        <ThemeView color='warning'  className='w-full h-4' />
+      </ThemeView>
+    </RippleContainer>
   );
 }
 
--- src/Components/Core/RippleContainer.tsx
+++ src/Components/Core/RippleContainer.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useState } from "react";
+import { ReactNode, useRef, useState } from "react";
 import { useThemeStore } from "../../Stores/Theme";
 import { Animated, GestureResponderEvent, Pressable, PressableProps, useAnimatedValue, View, ViewStyle } from "react-native";
 import { ColorVariant } from "../../Stores/Theme/types";
@@ -17,7 +17,7 @@ export type RippleContainerProps = PressableProps & {
 }
 
 
-export default function RippleContainer({children, style, onPress, color='primary', rippleColor, rippleOpacity=0.5, rippleScale=1, duration=300, rippleCount=3, ...props}: RippleContainerProps) {
+export default function RippleContainer({children, style, onPress, color='primary', rippleColor, rippleOpacity=0.4, rippleScale=1, duration=300, rippleCount=3, ...props}: RippleContainerProps) {
   
   const rgb = useThemeStore(s => s.colors[color].rgb.text.join(','));
   rippleColor ??= `rgb(${rgb})`;
@@ -26,10 +26,15 @@ export default function RippleContainer({children, style, onPress, color='primar
 
   const animatedValue = useAnimatedValue(0);
 
+  const button = useRef<View>(null);
 
   function handleOnPress(event: GestureResponderEvent) {
-    const {locationY: top, locationX: left} = event.nativeEvent;
-    setPosition({top, left});
+    const {pageX, pageY} = event.nativeEvent;
+    
+    button.current?.measure((x, y) => {
+      setPosition({top: pageY - y, left: pageX - x})
+    })
+
     startAnimation();
     
     onPress?.(event);
@@ -48,7 +53,7 @@ export default function RippleContainer({children, style, onPress, color='primar
 
 
   return (
-    <Pressable {...props} onPress={handleOnPress} style={[style, { overflow: 'hidden', position: 'relative' }]}>
+    <Pressable ref={button} {...props} onPress={handleOnPress} style={[style, { overflow: 'hidden', position: 'relative' }]}>
       <View className="absolute w-1/2 aspect-square" style={{...position, transform: 'translate(-50%, -50%)'}} >
         {
           [...new Array(Math.min(rippleCount, 5))].map((_, index) => (
@@ -56,15 +61,17 @@ export default function RippleContainer({children, style, onPress, color='primar
               className={'absolute w-full aspect-square rounded-full'}
               style={{ 
                 backgroundColor: rippleColor,
+                
 
                 opacity: animatedValue.interpolate({
                   inputRange: [0, 1],
-                  outputRange: [rippleOpacity - (index * 0.2), 0],
+                  outputRange: [rippleOpacity * ((rippleCount - index) / rippleCount), 0],
                 }),
 
                 transform: [{
                   scale: animatedValue.interpolate({
-                    inputRange: [0, 1], outputRange: [0, rippleScale - (index * 0.2)]
+                    inputRange: [0, 0.1, 1], 
+                    outputRange: [0, rippleScale * 0.1, rippleScale + (index * 0.1)]
                   })
                 }]
               }}
